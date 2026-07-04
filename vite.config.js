c7965864ef913f60c07d11e1ecb3a3909d7245e5
san-nico <diaz.morales.nicolas@gmail.com>
2026-07-03 20:29:34 -0400

    feat:install de gitpages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/G25-S1-temario-react/", // 👈 OJO: nombre exacto del repo
+  base: "/G26-S1-temario-react/", // 👈 OJO: nombre exacto del repo
 });
